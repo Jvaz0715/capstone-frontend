ff71e74877b3d6d7d986e3529341f3711d2fac74
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-01 14:42:09 -0500

    makes Auth Protected and Navbar files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import MainRouter from "./MainRouter";
+import Spinner from "./Spinner/Spinner";
+import AuthContextWrapper from "./context/AuthContext";
+
 import './App.css';
 
 function App() {
   return (
-    <div>Hello, World.</div>
+    <React.Suspense fallback={<Spinner />}>
+      <Router>
+        <AuthContextWrapper>
+          <MainRouter />
+        </AuthContextWrapper>
+      </Router>
+    </React.Suspense>
   );
-}
+};
 
 export default App;
---
+++ src/context/AuthContext.js
@@ -0,0 +1,48 @@
+import {
+   createContext,
+   useReducer
+} from "react";
+
+export const AuthContext = createContext({});
+
+const initialState = {
+   user: null,
+}
+
+const ACTIONS = {
+   LOGIN: "LOGIN",
+   LOGOUT: "LOGOUT"
+};
+
+function reducer(state, action) {
+   switch(action.type) {
+      case ACTIONS.LOGIN:
+         return {
+            user: {
+               email: action.user.email,
+               username: action.user.username,
+               isAuth: true,
+            }
+         };
+      case ACTIONS.LOGOUT:
+         return {
+            user: null,
+         };
+      default:
+         return;
+   };
+};
+
+function AuthContextWrapper({ children }) {
+   const [state, dispatch] = useReducer(reducer, initialState);
+
+   return (
+      <AuthContext.Provider
+         value={{state, dispatch}}
+      >
+         {children}
+      </AuthContext.Provider>
+   )
+};
+
+export default AuthContextWrapper;
