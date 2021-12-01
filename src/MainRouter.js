6dc9ffba8b4a3eb00fd89b4df0ebc9aee5475438
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-01 14:54:47 -0500

    Creates mainRouter

---
+++ src/MainRouter.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+} from 'react-router-dom';
+
+import Navbar from "./components/Navbar/Navbar";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+
+const Auth = React.lazy(() => import ("./components/Auth/Auth"));
+const Protected = React.lazy(() => import ("./components/Protected/Protected"));
+
+function MainRouter(props) {
+   return (
+      <Router>
+         {/* <Navbar /> */}
+         <Switch>
+            {/* <Route 
+               exact
+               path="/"
+               component={Home}
+            /> */}
+
+            <Route 
+               exact
+               path="/sign-up"
+               component={Auth}
+            />
+
+            <Route 
+               exact
+               path="/login"
+               component={Auth}
+            />
+
+            <PrivateRoute
+               exact
+               path="/protected"
+               component={Protected}
+            />
+
+            {/* <Route 
+               path="*"
+               exact 
+               component={NotFound}
+            /> */}
+
+         </Switch>
+      </Router>
+   )
+};
+
+export default MainRouter;
