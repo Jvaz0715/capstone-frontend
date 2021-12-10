2527a7e439055b4373f8ea122d8a115bfda623dc
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-10 09:49:54 -0500

    fixes results cards

--- src/components/Attractions/AttractionListItem.js
+++ src/components/Attractions/AttractionListItem.js
@@ -29,12 +29,11 @@ function AttractionSearchItem(props) {
 
    const distInMiles = convertedToMiles(dist);
    
-   
    return (
-      <Box sx={{ display: 'flex', flexDirection: "column", width: 400, marginBottom: 5 }}>
+      <Box sx={{ width: 400 }}>
          <Card sx={{ display: 'flex',}}>
-            <Box sx={{ display: 'flex', flexDirection: "row" }}>
-               <Box sx={{ display: 'flex', flexDirection: "column" }}>
+            <Box sx={{ display: 'flex', flexDirection: "row"}}>
+               <Box sx={{ display: 'flex', flexDirection: "column", width: "75%" }}>
                <CardContent sx={{ flex: '1 0 auto' }}>
                   <Typography component="div" variant="h5">
                      {name}
@@ -53,13 +52,13 @@ function AttractionSearchItem(props) {
                </Box>
                </Box>
             
-               <CardContent>
+               <CardContent sx={{border: "2px solid blue"}}>
                <img 
                   src="https://cdn-icons.flaticon.com/png/128/1862/premium/1862600.png?token=exp=1639080949~hmac=d6f5604cd09b073bce7d557ab332b01d"
                   alt="Theater"
                   width="70px"
                   height="70px"
-                  style={{border: "2px solid red"}}
+                  
                />
                </CardContent>
             </Box>
