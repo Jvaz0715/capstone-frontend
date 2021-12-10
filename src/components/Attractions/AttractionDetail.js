083e5aeccd03f1ad5650a7eb6bb3965fd4b3226b
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-10 11:48:00 -0500

    makes individual attraction details available

--- src/components/Attractions/AttractionDetail.js
+++ src/components/Attractions/AttractionDetail.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {
+   useState,
+   useEffect
+} from 'react';
+import axios from "axios";
 
 // TODO: create api hit function that will automatically load data
 // useState, useEffect
 // you don't need props because the data is going to come from the api hit on useEffect
 
-function AttractionDetail() {
+async function fetchAttractionDetails(xid) {
+   try {
+      const attractionDetails = await axios.get(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${process.env.REACT_APP_MAP_APIKEY}`);
+
+      console.log(attractionDetails)
+   } catch(e) {
+      console.log(e)
+   };
+};
+
+function AttractionDetail(props) {
+   const xid = props.match.params.xid;
+
+   useEffect(()=>{
+      fetchAttractionDetails(xid)
+   }, [])
    return (
       <div>
          Hello!
