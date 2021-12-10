d37a0a322c41f5eceb259af071df1c036f12e80e
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-10 14:29:30 -0500

    starts to add mui card

--- src/components/Attractions/AttractionDetail.js
+++ src/components/Attractions/AttractionDetail.js
@@ -8,27 +8,20 @@ function AttractionDetail(props) {
    const [attractionName, setAttractionName] = useState("");
    const [image, setImage] = useState("default image");
    const [attractionInfo, setAttractionInfo] = useState("default info");
-   const [externalURL, setExternalURL] = useState("");
-   const [wikiPage, setWikiPage] = useState("");
+   const [externalURL, setExternalURL] = useState("default externalURL");
+   const [wikiPage, setWikiPage] = useState("wikipage");
 
    const xid = props.match.params.xid;
-
-   // TODO: create mui card that will display the following information
-   /* 
-      name = attractionDetails.name
-      image = attractionDetails.preview.source 
-      information = attractionDetails.wikipedia_extracts.text
-      externalURL = attractionDetails.url
-      wikipediaPage = attractionDetails.wikipedia
-   */
 
    async function fetchAttractionDetails(xid) {
       try {
          const attractionDetails = await axios.get(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${process.env.REACT_APP_MAP_APIKEY}`);
-         console.log(attractionDetails.data)
+         
          setAttractionName(attractionDetails.data.name);
          attractionDetails.data.preview.source && setImage(attractionDetails.data.preview.source);
          attractionDetails.data.wikipedia_extracts.text && setAttractionInfo(attractionDetails.data.wikipedia_extracts.text);
+         attractionDetails.data.url && setExternalURL(attractionDetails.data.url);
+         attractionDetails.data.wikipedia && setWikiPage(attractionDetails.data.wikipedia);
 
       } catch(e) {
          console.log(e)
@@ -44,8 +37,8 @@ function AttractionDetail(props) {
          <span>Name: {attractionName}</span>
          <span>Image: {image}</span>
          <span>Info: {attractionInfo}</span>
-         {/* <span>ExternalURL: {attractionDetails.url && attractionDetails.url}</span> */}
-         {/* <span>Wikipedia Page: {attractionDetails.wikipedia && attractionDetails.wikipedia}</span> */}
+         <span>ExternalURL: {externalURL}</span>
+         <span>Wikipedia Page: {wikiPage}</span>
       </div>
    )
 };
