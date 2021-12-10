86802e02c599a0a4d8a61812f09656b9665889c9
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-10 11:57:46 -0500

    has pages working

--- src/components/Attractions/AttractionDetail.js
+++ src/components/Attractions/AttractionDetail.js
@@ -8,25 +8,32 @@ import axios from "axios";
 // useState, useEffect
 // you don't need props because the data is going to come from the api hit on useEffect
 
-async function fetchAttractionDetails(xid) {
-   try {
-      const attractionDetails = await axios.get(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${process.env.REACT_APP_MAP_APIKEY}`);
-
-      console.log(attractionDetails)
-   } catch(e) {
-      console.log(e)
-   };
-};
-
 function AttractionDetail(props) {
+   const [attractionDetails, setAttractionDetails] = useState({})
    const xid = props.match.params.xid;
+
+   async function fetchAttractionDetails(xid) {
+      try {
+         const attractionDetails = await axios.get(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${process.env.REACT_APP_MAP_APIKEY}`);
+   
+         setAttractionDetails(attractionDetails.data)
+   
+      } catch(e) {
+         console.log(e)
+      };
+   };
 
    useEffect(()=>{
       fetchAttractionDetails(xid)
-   }, [])
+   }, []);
+
+
+   console.log('attractionDetails')
+   console.log(attractionDetails)
+
    return (
       <div>
-         Hello!
+         Hello! {attractionDetails.name}
       </div>
    )
 };
