06393ff4d61401dccec82670fb90ff8e5bda3eca
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-10 11:01:11 -0500

    Fixes layout

--- src/components/Attractions/AttractionListItem.js
+++ src/components/Attractions/AttractionListItem.js
@@ -34,35 +34,35 @@ function AttractionSearchItem(props) {
          <Card sx={{ display: 'flex',}}>
             <Box sx={{ display: 'flex', flexDirection: "row"}}>
                <Box sx={{ display: 'flex', flexDirection: "column", width: "75%" }}>
-               <CardContent sx={{ flex: '1 0 auto' }}>
-                  <Typography component="div" variant="h5">
-                     {name}
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary" component="div">
-                     {distInMiles} miles away
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary" component="div">
-                     {rate}/5 rating
-                  </Typography>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
+                     <Typography component="div" variant="h5">
+                        {name}
+                     </Typography>
+                     <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {distInMiles} miles away
+                     </Typography>
+                     <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {rate}/5 rating
+                     </Typography>
+                  </CardContent>
+
+                  {/* put buttons */}
+                  <Box>
+                     <button>Learn more</button>
+                  </Box>
+
+               </Box>
+               
+               <CardContent sx={{border: "2px solid blue"}}>
+                  <img 
+                     src="https://cdn-icons.flaticon.com/png/128/1862/premium/1862600.png?token=exp=1639080949~hmac=d6f5604cd09b073bce7d557ab332b01d"
+                     alt="Theater"
+                     width="70px"
+                     height="70px"
+                  />
                </CardContent>
 
-               <Box>
-               {/* put buttons */}
-                  <button>Learn more</button>
-               </Box>
-               </Box>
-            
-               <CardContent sx={{border: "2px solid blue"}}>
-               <img 
-                  src="https://cdn-icons.flaticon.com/png/128/1862/premium/1862600.png?token=exp=1639080949~hmac=d6f5604cd09b073bce7d557ab332b01d"
-                  alt="Theater"
-                  width="70px"
-                  height="70px"
-                  
-               />
-               </CardContent>
             </Box>
-            
          </Card>
       </Box>
    );
